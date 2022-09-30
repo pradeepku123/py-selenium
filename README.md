9ebbf2fd1d960019bd072fa82a9385d5ba4e9b93
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2022-09-30 19:42:39 +0530

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Setup Python Project for Selenium Explore
+# Setup WebdriverManager & Dynamically download Driver
+# Extract Web Element & fill data
---
+++ src/test/test_start.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+def test_start():
+    driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get('http://localhost:3000')
+    driver.find_element(By.CSS_SELECTOR,'#username').send_keys('Katharina_Bernier')
+    driver.find_element(By.CSS_SELECTOR,'#password').send_keys('s3cret')
+    time.sleep(5)
+    
