302908bd17fbb01b8b0594afcc53f6d76750b938
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2022-10-25 21:35:47 +0530

    implicit Wait

--- src/test/test_start.py
+++ src/test/test_start.py
@@ -7,7 +7,11 @@ import time
 def test_start():
     """ Start up python Selenium"""
     driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    # implicit wait for Chrome
+    driver.implicitly_wait(10) # default wait 0 we set industries standard timeout 10 Seconds
     driver.get('http://localhost:3000')
+    # maximize_window implementation
+    driver.maximize_window()
     driver.find_element(By.CSS_SELECTOR,'#username').send_keys('Katharina_Bernier')
     driver.find_element(By.CSS_SELECTOR,'#password').send_keys('s3cret')
     driver.refresh()
